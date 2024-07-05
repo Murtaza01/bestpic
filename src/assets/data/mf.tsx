5f39ede051142ce9e5e53940cfc313f64f64a8e3
Murtaza01 <merdada2000@hotmail.com>
2024-07-06 00:21:21 +0300

    forgot some imports which made the build crash

--- src/assets/data/mf.tsx
+++ src/assets/data/mf.tsx
@@ -1,7 +1,7 @@
-import img1 from "../images/1.jpg";
-import img2 from "../images/2.jpg";
-import img3 from "../images/3.jpg";
-import img4 from "../images/4.jpg";
+import img1 from "../images/test.jpg";
+import img2 from "../images/test2.jpg";
+import img3 from "../images/test3.jpg";
+import img4 from "../images/test4.jpg";
 const mfData = {
   mohamed: [img1, img2, img3, img4],
   fatima: [img2, img1, img4, img3],
